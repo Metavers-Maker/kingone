1cc09e9fef115cdada6e8dcfc3f7b9b7e1937338
a7freeze <apple7.sve@gmail.com>
2023-08-14 21:02:59 +0800

    add ignore file .add home sc and loading sc

---
+++ assets/scripts/home/HomeDirector.ts
@@ -0,0 +1,25 @@
+
+import { _decorator, Component, Node, ScrollView, UITransform, Vec3 } from 'cc';
+const { ccclass, property, type } = _decorator;
+
+@ccclass('HomeDirector')
+export class HomeDirector extends Component {
+
+    protected onLoad(): void {
+        //
+    }
+
+    start () {
+        // this.scroll.on(Node.EventType.SIZE_CHANGED, this.sizeChanged, this);
+    }
+
+    protected onDestroy(): void {
+        //
+    }
+
+
+    update (deltaTime: number) {
+        // [4]
+    }
+}
+
